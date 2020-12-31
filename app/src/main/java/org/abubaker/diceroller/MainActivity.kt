298e0385edf97fe6c1569575944859563fa139d0
abubaker <mohammad_abubaker@yahoo.com>
2020-12-31 21:59:19 +0500

    added rollDice() function

--- app/src/main/java/org/abubaker/diceroller/MainActivity.kt
+++ app/src/main/java/org/abubaker/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package org.abubaker.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +21,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener{
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val randInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randInt.toString()
     }
 
 }
