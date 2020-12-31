cca0fbc5c05f23c335cc2bea995b3dab346492e4
abubaker <mohammad_abubaker@yahoo.com>
2020-12-31 23:06:53 +0500

    Added functionality to show images instead of updating text for the rolled number.

--- app/src/main/java/org/abubaker/diceroller/MainActivity.kt
+++ app/src/main/java/org/abubaker/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package org.abubaker.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -27,8 +27,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randInt.toString()
+        // val resultText: TextView = findViewById(R.id.result_text)
+        // resultText.text = randInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        // When = Switch Case
+        val drawableResource = when (randInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Assign
+        diceImage.setImageResource(drawableResource)
     }
 
 }
