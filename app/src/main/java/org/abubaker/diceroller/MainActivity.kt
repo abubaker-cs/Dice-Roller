ac9615674919fae02ba8374e82749dd8cf50d01d
abubaker <mohammad_abubaker@yahoo.com>
2020-12-31 20:04:13 +0500

    Changed "Hello World!" to "Hello Android!"

--- app/src/main/java/org/abubaker/diceroller/MainActivity.kt
+++ app/src/main/java/org/abubaker/diceroller/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // onCreate - LifeTime
         super.onCreate(savedInstanceState)
+
+        // Inflate Selected View and Draw it to the screen:
         setContentView(R.layout.activity_main)
     }
+
 }
