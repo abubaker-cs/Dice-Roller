cb8c58956173cb69161383151fa7abd47967b62d
abubaker <mohammad_abubaker@yahoo.com>
2020-12-31 21:09:44 +0500

    Added Toast message

--- app/src/main/java/org/abubaker/diceroller/MainActivity.kt
+++ app/src/main/java/org/abubaker/diceroller/MainActivity.kt
@@ -2,6 +2,8 @@ package org.abubaker.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         // Inflate Selected View and Draw it to the screen:
         setContentView(R.layout.activity_main)
+
+        // Select the Button from the activity_main.xml
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
